0bc9e1259b461619cb5351694847f26c0c36b87a
miseri <miseri.osk12@gmail.com>
2021-03-08 04:13:26 +0900

    add topButton

---
+++ Tinder/Home/HomeViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Tinder
+//
+//  Created by OsakaMiseri on 2021/02/18.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+
+        let topView = TopView()
+
+        let view2 = UIView()
+        view2.backgroundColor = .blue
+
+        let bottomView = BottomView()
+
+        let stackView = UIStackView(arrangedSubviews: [topView, view2, bottomView])
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        self.view.addSubview(stackView)
+
+        [
+            topView.heightAnchor.constraint(equalToConstant: 100),
+            bottomView.heightAnchor.constraint(equalToConstant: 120),
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            stackView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            stackView.rightAnchor.constraint(equalTo: view.rightAnchor)
+        ]
+        .forEach { $0.isActive = true }
+    }
+
+}
